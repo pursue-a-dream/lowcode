59472dabc5c776903142812f1524d99e896ef8c0
lalala-1-s <1131736528@qq.com>
2024-04-26 09:35:49 +0800

    修改eslint配置信息

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,5 +4,5 @@ module.exports = {
   arrowParens: 'avoid', // 箭头函数单个参数不加分号
   singleQuote: true, // 使用单引号
   printWidth: 110, // 换行长度
-  endOfLine: 'crlf' // 换行格式采用 CRLF。说明：https://www.jianshu.com/p/b03ad01acd69
+  endOfLine: 'crlf', // 换行格式采用 CRLF。说明：https://www.jianshu.com/p/b03ad01acd69
 }
